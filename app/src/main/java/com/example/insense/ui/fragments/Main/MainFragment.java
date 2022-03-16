3643dc2b7cd92417a5b1433936f3a3115742add3
varvara <varia.mazurovaa@mail.ru>
2022-03-16 23:58:43 +0300

    sorry

--- app/src/main/java/com/example/insense/ui/fragments/Main/MainFragment.java
+++ app/src/main/java/com/example/insense/ui/fragments/Main/MainFragment.java
@@ -3,64 +3,36 @@ package com.example.insense.ui.fragments.Main;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.insense.R;
+import com.example.insense.databinding.FragmentMainBinding;
+/*import com.example.insense.databinding.FragmentMainBindingImpl;*/
+import com.example.insense.ui.fragments.Binding.BindingFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MainFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MainFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
-        MainFragment fragment = new MainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    private FragmentMainBinding fragmentMainBinding;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        fragmentMainBinding = FragmentMainBinding.inflate(inflater, container, false);
+        fragmentMainBinding.buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(MainFragment.this).navigate(R.id.action_mainFragment_to_settingsFragment);
+            }
+        });
+        return fragmentMainBinding.getRoot();
     }
 }
--- app/src/main/java/com/example/insense/ui/fragments/Settings/SettingsFragment.java
+++ app/src/main/java/com/example/insense/ui/fragments/Settings/SettingsFragment.java
@@ -1,4 +1,4 @@
-package com.example.insense.ui.fragments.Profile;
+package com.example.insense.ui.fragments.Settings;
 
 import android.os.Bundle;
 
@@ -11,7 +11,8 @@ import android.view.ViewGroup;
 import com.example.insense.R;
 
 
-public class ProfileFragment extends Fragment {
+public class SettingsFragment extends Fragment {
+
 
 
     @Override
@@ -24,6 +25,6 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
